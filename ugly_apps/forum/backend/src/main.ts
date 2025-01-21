4d8ba5482b6a5feb8c84b1634304e363a453b742
murirop <panashemuriro2021@gmail.com>
2025-01-21 16:59:04 -0600

    allow cors

--- ugly_apps/forum/backend/src/main.ts
+++ ugly_apps/forum/backend/src/main.ts
@@ -1,8 +1,24 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors()
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // Frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to send cookies or HTTP authentication
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
